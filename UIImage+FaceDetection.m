a75bcf711387cc9c37d9038189cba51bf1bfe8d0
Tom Hutchinson <tomhut101@gmail.com>
2013-07-03 14:54:04 +0100

    Removed redundant code

--- UIImage+FaceDetection.m
+++ UIImage+FaceDetection.m
@@ -4,18 +4,12 @@
 
 - (NSArray *)facesWithAccuracy :(NSString *)detectorAccuracy {
     CIImage *coreImageRepresentation = [[CIImage alloc] initWithImage:self];
-    
+
     CIDetector* detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:[NSDictionary dictionaryWithObject:detectorAccuracy forKey:CIDetectorAccuracy]];
-    
+
     NSArray *features = [detector featuresInImage:coreImageRepresentation];
-    
-    NSMutableArray *faces = [[NSMutableArray alloc] init];
-    
-    for (CIFaceFeature *face in features) {
-        [faces addObject:face];
-    }
-    
-    return [faces copy];
+
+    return features;
 }
 
 - (CIFaceFeature *)largestFaceWithAccuracy :(NSString *)detectorAccuracy {
